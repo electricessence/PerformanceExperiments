fa88486d567ad26434185d52d3540dea34f1b59a
electricessence <5899455+electricessence@users.noreply.github.com>
2023-03-13 17:22:17 -0700

    Added ToStringD test.

---
+++ PerformanceExperiments/EnumToStringDTests.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace PerformanceExperiments;
+
+[MemoryDiagnoser]
+public class EnumToStringDTests
+{
+	private static readonly IReadOnlyList<Greek> Values = Array.AsReadOnly(Enum.GetValues<Greek>());
+
+	[Benchmark]
+	public string IntToString()
+	{
+		var s = "";
+		foreach(var e in Values)
+		{
+			var i = (int)e;
+			s = i.ToString();
+		}
+
+		return s;
+	}
+
+	[Benchmark]
+	public string ToStringD()
+	{
+		var s = "";
+		foreach (var e in Values)
+			s = e.ToString("D");
+
+		return s;
+	}
+
+	[Benchmark]
+	public string ToIntString()
+	{
+		var s = "";
+		foreach (var e in Values)
+			s = ToIntString(e);
+
+		return s;
+	}
+
+
+	private static string ToIntString<T>(T e)
+		where T : Enum
+	{
+		var i = (int)Convert.ChangeType(e, typeof(int));
+		return i.ToString();
+	}
+}
